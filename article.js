f3a9a10da258725945dd8ff6e284a3e67ac8152b
Riley Mathews <rileymathews80@gmail.com>
2018-04-06 13:57:31 -0500

    added framework for building out article

---
+++ article.js
@@ -0,0 +1,4 @@
+//app to create article tag and insert platform data
+const article = document.createElement("article")
+article.setAttribute("id", "5")
+document.querySelector("section").appendChild(article)
